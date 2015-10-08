01de2f465c4755aadf98397d84f0b864aad0f7a0
sunban <sunbandev@iclude.com>
2015-10-08 11:57:09 +0800

    这是我的第一个仓库文件

---
+++ Demo02-gitMyRepository/Demo02-gitMyRepository/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Demo02-gitMyRepository
+//
+//  Created by tarena on 15/10/8.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"阿萨德好哦啊佛啊佛山佛啊是");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
